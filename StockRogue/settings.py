0f03150cd05a90a2cd68d3b0c99c60fef691e223
oroko123 <pperkowski96@gmail.com>
2017-05-16 00:48:03 +0200

    Update settings.py
    próba staticów

--- StockRogue/settings.py
+++ StockRogue/settings.py
@@ -146,18 +146,16 @@ STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 ]
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.11/howto/static-files/
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'staticfiles'),
+    os.path.join(PROJECT_ROOT, 'static'),
 )
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
