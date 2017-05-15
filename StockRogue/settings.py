90a7cd4a69b7fb0b224d3cd246dfedd47c26a58f
zoltus123 <mikolaj@leonarski.pl>
2017-05-15 23:25:45 +0200

    O jedno zero było za mało przy sprawdzaniu daty

--- StockRogue/settings.py
+++ StockRogue/settings.py
@@ -141,6 +141,11 @@ USE_L10N = True
 
 USE_TZ = True
 
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
@@ -152,7 +157,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 )
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
