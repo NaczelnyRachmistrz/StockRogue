6aa3189d67a6177622907ac7493fef375313577d
zoltus123 <mikolaj@leonarski.pl>
2017-05-15 22:37:34 +0200

    O jedno zero było za mało przy sprawdzaniu daty

--- stock_rogue_app/management/commands/database_set_up.py
+++ stock_rogue_app/management/commands/database_set_up.py
@@ -45,7 +45,8 @@ class Command(BaseCommand):
                 insert_list = []
                 for row in reader:
                     if row[0] != "<TICKER>":
-                        if int(row[1]) > 2010000:
+                        #Było o jedno zero za mało
+                        if int(row[1]) > 2010 * 100 * 100:
                             print(row[0] + " " + row[1])
 
                             s, created = Spolka.objects.get_or_create(
