afe69aa9ed47f433e2920a1ff4e19c35f98ae70d
zoltus123 <mikolaj@leonarski.pl>
2017-05-16 16:09:47 +0200

    Static - kolejna próba

--- StockRogue/settings.py
+++ StockRogue/settings.py
@@ -147,7 +147,7 @@ STATICFILES_FINDERS = [
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'
 
