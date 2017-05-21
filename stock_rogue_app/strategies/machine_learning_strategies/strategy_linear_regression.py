7986231516d32b13f034d7db14ddc9ab1e85ba09
Michał Kukuła <john@example.com>
2017-05-21 22:44:40 +0200

    Linear Regression w prostej wersji dodane

--- stock_rogue_app/strategies/machine_learning_strategies/strategy_linear_regression.py
+++ stock_rogue_app/strategies/machine_learning_strategies/strategy_linear_regression.py
@@ -4,12 +4,9 @@ from sklearn import linear_model
 
 from . import vector_preprocess
 
+from . import auxiliary_functions
 
-DEFAULT_TABLE = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 14, 16, 18, 21, 24, 27, 30, 40, 50,
-                 60, 80, 100, 120, 130, 150, 160, 170, 180, 220, 260, 300, 400, 500, 600,
-                 750, 850, 1000, 1250, 1500]
-
-def predict_future(company_data, result, days_past = DEFAULT_TABLE):
+def predict_future(company_data, result, days_past, wig_data=True):
     '''Prosta strategia wykorzystująca regresję liniową. Przyjmuje za czynniki
         zmiany kursu w stosunku do wartości sprzed liczby dni podanych w tablicy days_past'''
 
@@ -25,6 +22,18 @@ def predict_future(company_data, result, days_past = DEFAULT_TABLE):
                                                                       days_past,
                                                                       el)
         lin_model[el] = linear_model.LinearRegression()
+
+    if wig_data:
+        X_matrix_wig = {}
+        y_vector_wig = {}
+        wig = auxiliary_functions.collect_wig_data()
+        for el in FIELDS:
+            X_matrix_wig[el], y_vector_wig[el] = vector_preprocess.vectorize_data(wig,
+                                                             days_past,
+                                                             el)
+
+            X_matrix[el] += X_matrix_wig[el]
+            y_vector[el] += y_vector_wig[el]
 
     X_matrix_combined = [X_matrix[FIELDS[0]][i] +
                          X_matrix[FIELDS[1]][i] +
@@ -55,6 +64,10 @@ def predict_future(company_data, result, days_past = DEFAULT_TABLE):
             day[el] = lin_model[el].predict(X_vector_combined)[0]
             day[el] *= value
 
+        if day["kurs_min"] > day["kurs_max"]:
+            day["kurs_min"] = day["kurs_max"]
+            day["kurs_biezacy"] = day["kurs_min"]
+
 
         company_data_copy = [day] + company_data_copy
 
@@ -62,5 +75,53 @@ def predict_future(company_data, result, days_past = DEFAULT_TABLE):
     result += {}
     return result
 
+
+def raw_predict_future(company_data, result, days_past):
+    '''Prosta strategia wykorzystująca regresję liniową. Przyjmuje za czynniki
+        zmiany kursu w stosunku do wartości sprzed liczby dni podanych w tablicy days_past'''
+
+    FIELDS = ("kurs_min", "kurs_max", "kurs_biezacy")
+    X_matrix = {}
+    y_vector = {}
+    lin_model = {}
+
+    company_data_copy = list(company_data)
+
+    for el in FIELDS:
+        X_matrix[el], y_vector[el] = vector_preprocess.raw_vectorize_data(company_data,
+                                                                          days_past,
+                                                                          el)
+        lin_model[el] = linear_model.LinearRegression()
+
+    X_matrix_combined = [X_matrix[FIELDS[0]][i] +
+                         X_matrix[FIELDS[1]][i] +
+                         X_matrix[FIELDS[2]][i]
+                         for i in range(len(X_matrix[FIELDS[0]]))]
+
+    for el in FIELDS:
+        lin_model[el].fit(X_matrix_combined,
+                          y_vector[el])
+
+
+    X_vector = {}
+    for day in result:
+        X_vector.clear()
+        for el in FIELDS:
+            X_vector[el] = vector_preprocess.change_days(company_data_copy,
+                                                  el,
+                                                  days_past)
+
+        X_vector_combined = X_vector[FIELDS[0]] + \
+                            X_vector[FIELDS[1]] + \
+                            X_vector[FIELDS[2]]
+
+        for el in  FIELDS:
+            day[el] = lin_model[el].predict(X_vector_combined)[0]
+
+        company_data_copy = [day] + company_data_copy
+
+    #TODO Dodatkowy rekord, do wywalenia będzie
+    result += {}
+    return result
 
 
