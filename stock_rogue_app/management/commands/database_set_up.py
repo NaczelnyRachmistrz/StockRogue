31cf89508559147919c892352bee3a568b3a3c75
zoltus123 <mikolaj@leonarski.pl>
2017-05-27 17:30:34 +0200

    notowania z bazy danych

--- stock_rogue_app/management/commands/database_set_up.py
+++ stock_rogue_app/management/commands/database_set_up.py
@@ -45,8 +45,8 @@ class Command(BaseCommand):
                 insert_list = []
                 for row in reader:
                     if row[0] != "<TICKER>":
-                        #Pobieramy notowania spółek od 1 kwietnia 2017 roku
-                        if int(row[1]) > 20170400:
+                        #Pobieramy notowania spółek od 22 maja 2017 roku
+                        if int(row[1]) > 20170521:
                             print(row[0] + " " + row[1])
 
                             s, created = Spolka.objects.get_or_create(
